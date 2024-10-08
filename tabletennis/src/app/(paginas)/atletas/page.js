c0f395704dc90bd22314209b9e4c1f13d127559a
HeizoAlencar <heizosilva@gmail.com>
2024-10-07 21:44:19 -0300

    work from 07.10.24

---
+++ tabletennis/src/app/(paginas)/atletas/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Atletas() {
+  return (
+    <div className="bg-violet-950 h-100vw w-100vw">
+
+    </div>
+  );
+}
+  
